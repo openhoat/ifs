75e063e9ef12f3ea96cbe88aba5ab8f03573f99e
Olivier Penhoat <openhoat@gmail.com>
2013-01-01 19:37:10 +0100

    debug

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var path = require('path')
+  , fs = require('fs')
   , config = require(path.join(__dirname, 'config.js'))
   , wbp = require('wbpjs');
 
-wbp.start(config);
+wbp.configure(config);
+
+var mvcPlugin = wbp.findPlugin('wbpjs-mvc');
+var downloadPath = path.join(mvcPlugin.config.publicDir, 'download')
+if (!fs.existsSync(downloadPath)) {
+  wbp.util.mkdirp(downloadPath);
+}
+
+wbp.start();
