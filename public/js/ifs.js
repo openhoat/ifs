3b92635469f6807474d5e69350c44d222ef4ffc4
Olivier Penhoat <openhoat@gmail.com>
2013-01-21 07:46:16 +0100

    wbpjs update

--- public/js/ifs.js
+++ public/js/ifs.js
@@ -1,17 +1,24 @@
-function hasCssClass(el,cssClass) {
+function hasCssClass(el, cssClass) {
   return el.className.match(new RegExp('(\\s|^)' + cssClass + '(\\s|$)'));
 }
-function addCssClass(el,cssClass) {
-  if (hasCssClass(el,cssClass)) { return false; }
+
+function addCssClass(el, cssClass) {
+  if (hasCssClass(el, cssClass)) {
+    return false;
+  }
   el.className = el.className + ' ' + cssClass;
   return true;
 }
-function removeCssClass(el,cssClass) {
-  if (!hasCssClass(el,cssClass)) { return false; }
+
+function removeCssClass(el, cssClass) {
+  if (!hasCssClass(el, cssClass)) {
+    return false;
+  }
   var reg = new RegExp('(\\s|^)' + cssClass + '(\\s|$)');
-  el.className = el.className.replace(reg,' ');
+  el.className = el.className.replace(reg, ' ');
   return true;
 }
+
 function dragAndDropSupported() {
   var div = document.createElement('div');
   return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
